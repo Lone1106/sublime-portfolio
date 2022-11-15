b00c96d57c7e082b4f3fcff33e3ac795dbe995ff
Lone1106 <97834596+Lone1106@users.noreply.github.com>
2022-11-15 16:08:21 +0100

    Started projects sectiom, fixed bugs

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,98 @@
 import classes from "./Projects.module.css";
+import Placeholder from "./img/Placeholder.png";
 
 function Projects() {
     return (
         <section className={classes.projects}>
-            <h2>My work</h2>
+            <h2>See my recent work</h2>
+            <figure className={classes.workItems}>
+                <div className={classes.work}>
+                    <img
+                        className={classes.image}
+                        src={Placeholder}
+                        alt="Mockup"
+                        loading="lazy"
+                    />
+                    <div className={classes.info}>
+                        <h4>Flashcard App</h4>
+                        <p className={classes.tech}>
+                            HTML, CSS, React, Firebase
+                        </p>
+                        <div className={classes.links}>
+                            <a
+                                className={classes.link}
+                                href="https://github.com/Lone1106/flashcard-app"
+                            >
+                                Github
+                            </a>
+                            <a
+                                className={classes.link}
+                                href="https://harmonious-platypus-6d8124.netlify.app/home"
+                            >
+                                Demo
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={classes.work}>
+                    <img
+                        className={classes.image}
+                        src={Placeholder}
+                        alt="Mockup"
+                        loading="lazy"
+                    />
+                    <div className={classes.info}>
+                        <h4>Flashcard App</h4>
+                        <p className={classes.tech}>
+                            HTML, CSS, React, Firebase
+                        </p>
+                        <div className={classes.links}>
+                            <a
+                                className={classes.link}
+                                href="https://github.com/Lone1106/flashcard-app"
+                            >
+                                Github
+                            </a>
+                            <a
+                                className={classes.link}
+                                href="https://harmonious-platypus-6d8124.netlify.app/home"
+                            >
+                                Demo
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={classes.work}>
+                    <img
+                        className={classes.image}
+                        src={Placeholder}
+                        alt="Mockup"
+                        loading="lazy"
+                    />
+                    <div className={classes.info}>
+                        <h4>Flashcard App</h4>
+                        <p className={classes.tech}>
+                            HTML, CSS, React, Firebase
+                        </p>
+                        <div className={classes.links}>
+                            <a
+                                className={classes.link}
+                                href="https://github.com/Lone1106/flashcard-app"
+                            >
+                                Github
+                            </a>
+                            <a
+                                className={classes.link}
+                                href="https://harmonious-platypus-6d8124.netlify.app/home"
+                            >
+                                Demo
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </figure>
         </section>
     );
 }
